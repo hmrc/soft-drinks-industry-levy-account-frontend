4ba88139f6f8bef37a4bd4d6ece9f236ed700639
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-04-18 16:26:26 +0100

    DLS-10353: soft-drinks-industry-levy-account-frontend page

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -21,15 +21,13 @@ import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.IndexView
 
 class IndexController @Inject()(
                                  val controllerComponents: MessagesControllerComponents,
-                                 identify: AuthenticatedAction,
-                                 view: IndexView
+                                 identify: AuthenticatedAction
                                ) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = identify { implicit request =>
-    Ok(view())
+    Redirect(routes.ServicePageController.onPageLoad)
   }
 }
