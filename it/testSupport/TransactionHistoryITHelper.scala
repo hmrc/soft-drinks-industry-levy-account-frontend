44bdbc6e04dfc67e213af2cb725955d5e56a4794
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2024-07-18 15:20:04 +0100

    DLS-10517 added old sdil routes that route to account-frontend routes

--- it/testSupport/TransactionHistoryITHelper.scala
+++ it/testSupport/TransactionHistoryITHelper.scala
@@ -141,7 +141,7 @@ trait TransactionHistoryITHelper extends ControllerITTestHelper {
 
     val homeLink = page.getElementById("returnToServicePage")
     homeLink.text() mustBe "You can go to your Soft Drinks Industry Levy account"
-    homeLink.attr("href") mustBe "/soft-drinks-industry-levy-account-frontend"
+    homeLink.attr("href") mustBe "/soft-drinks-industry-levy-account-frontend/home"
   }
 
   private def expectedDateField(transactionHistoryItem: TransactionHistoryItem): String = {
