e22a72dfc96dba14eac136d4d31f72b0637efaa7
jawadsaleem5747 <175114233+jawadsaleem5747@users.noreply.github.com>
2025-09-16 12:22:06 +0100

    DLS-12110: [SDIL] Replace playSuffix with playVersion in soft-drinks-industry-levy-account-frontend AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,25 +2,25 @@ import sbt._
 
 object AppDependencies {
 
-  private val playSuffix = s"-play-30"
+  private val playVersion = s"-play-30"
   private val bootstrapVersion = "9.9.0"
   private val hmrcMongoVersion = "2.5.0"
   private val playFrontendHMRCVersion = "11.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playSuffix" % playFrontendHMRCVersion,
-    "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix" % bootstrapVersion,
-    "uk.gov.hmrc"       %% s"play-conditional-form-mapping$playSuffix" % "3.2.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playVersion" % playFrontendHMRCVersion,
+    "uk.gov.hmrc"       %% s"bootstrap-frontend$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping$playVersion" % "3.2.0",
     "org.typelevel"     %% "cats-core"                     % "2.12.0",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"         % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% s"crypto-json$playSuffix"        % "8.2.0"
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playVersion"         % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% s"crypto-json$playVersion"        % "8.2.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.jsoup"               %  "jsoup"                        % "1.18.1",
-    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"    % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"   % hmrcMongoVersion,
+    "uk.gov.hmrc"             %% s"bootstrap-test$playVersion"    % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playVersion"   % hmrcMongoVersion,
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"        % "1.1.0"
   ).map(_ % "test, it")
 
