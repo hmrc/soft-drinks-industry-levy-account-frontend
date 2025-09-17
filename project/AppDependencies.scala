38d3922df9b31c3e7d59df10f7d68bdbe5842f66
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2025-09-17 09:32:53 +0100

    DLS-12127:[SDIL] Fix Platops PR Bot suggestions for SDIL Account FE

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = s"-play-30"
-  private val bootstrapVersion = "9.9.0"
+  private val bootstrapVersion = "9.19.0"
   private val hmrcMongoVersion = "2.7.0"
   private val playFrontendHMRCVersion = "12.13.0"
 
@@ -12,13 +12,13 @@ object AppDependencies {
     "uk.gov.hmrc"       %% s"play-frontend-hmrc$playVersion" % playFrontendHMRCVersion,
     "uk.gov.hmrc"       %% s"bootstrap-frontend$playVersion" % bootstrapVersion,
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping$playVersion" % "3.3.0",
-    "org.typelevel"     %% "cats-core"                     % "2.12.0",
+    "org.typelevel"     %% "cats-core"                     % "2.13.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playVersion"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% s"crypto-json$playVersion"        % "8.3.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"               %  "jsoup"                        % "1.18.1",
+    "org.jsoup"               %  "jsoup"                        % "1.21.2",
     "uk.gov.hmrc"             %% s"bootstrap-test$playVersion"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playVersion"   % hmrcMongoVersion,
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"        % "1.1.0"
